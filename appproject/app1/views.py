0baf4bc05cacfcffa5788b9bc884693e19dfbe02
Philipp <155749569+Firsthum4n@users.noreply.github.com>
2024-07-31 16:52:12 +0300

    Add files via upload

---
+++ appproject/app1/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from app1.models import Player
+
+
+def index_page(request):
+
+    # new_player = Player(name='jinja')
+    # new_player.save()
+
+    all_players = Player.objects.all()
+    print(all_players)
+
+
+
+    return render(request, 'index.html')
