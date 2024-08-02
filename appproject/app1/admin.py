6a2143ee690f8de1e1cf82d95ba6630db738119b
Jan Philipp <zanfilipp259@gmail.com>
2024-08-02 12:59:57 +0300

    new

--- appproject/app1/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from app1.models import Player
-
-
-admin.site.register(Player)
--- appproject/appproject/urls.py
+++ appproject/appproject/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from app1.views import index_page
+from django.urls import path, include
+from main.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index_page)
+    path('', include('main.urls'))
 ]
