3580fa3d6ab15826bd5eb778d2de3790d10dce90
Zan <zanfilipp259@gmail.com>
2024-08-05 02:28:03 +0300

    12

--- appproject/main/forms.py
+++ appproject/main/forms.py
@@ -7,11 +7,11 @@ from .models import *
 
 
 class RegisterUserForm(UserCreationForm):
+    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+
     class Meta:
         model = User
         fields = ('username', 'password1', 'password2')
-        widgets = {
-            'username': forms.TextInput(attrs={'class': 'form-control'}),
-            'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
-            'password2': forms.PasswordInput(attrs={'class': 'form-control'}),
-                   }
+
